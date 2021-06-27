07d99a87202961daf2de60a801153b83846e68b0
Ethan Kim <ethan_kim@college.harvard.edu>
2021-06-27 17:57:47 -0600

    upload data script

--- data/download_data.sh
+++ data/download_data.sh
@@ -3,9 +3,12 @@
 echo "Downloading Ultrasuite Data"
 
 # Download wav files
+mkdir data 
+cd data
 mkdir td
 cd td
 mkdir wav
+cd wav
 rsync -av ultrasuite-rsync.inf.ed.ac.uk::ultrasuite/core-uxtd/core/*/*.wav .
 cd ../
 mkdir transcripts
@@ -17,13 +20,13 @@ mkdir ssd
 cd ssd
 mkdir wav
 cd wav 
-rsync -av ultrasuite-rsync.inf.ed.ac.uk::ultrasuite/core-uxssd/core/*/*.wav .
-rsync -av ultrasuite-rsync.inf.ed.ac.uk::ultrasuite/core-upxd/core/*/*.wav .
+rsync -av  ultrasuite-rsync.inf.ed.ac.uk::ultrasuite/core-uxssd/core/*/*/*.wav .
+rsync -av ultrasuite-rsync.inf.ed.ac.uk::ultrasuite/core-upx/core/*/*/*.wav .
 cd ../
 mkdir transcripts
 cd transcripts
-rsync -av ultrasuite-rsync.inf.ed.ac.uk::ultrasuite/core-uxssd/core/*/*.txt .
-rsync -av ultrasuite-rsync.inf.ed.ac.uk::ultrasuite/core-upxd/core/*/*.txt .
-cd ../../
+rsync -av ultrasuite-rsync.inf.ed.ac.uk::ultrasuite/core-uxssd/core/*/*/*.txt .
+rsync -av ultrasuite-rsync.inf.ed.ac.uk::ultrasuite/core-upx/core/*/*/*.txt .
+cd ../../../
 
 
